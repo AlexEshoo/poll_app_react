e0ffae4e306e20bd6c856579b210274b2ec0de79
Alex Eshoo <AlexEshoo@gmail.com>
2020-05-23 18:05:55 -0400

    converted pollform to functional component

--- src/Components/PollVoteForm.js
+++ src/Components/PollVoteForm.js
@@ -1,61 +1,44 @@
 import React from "react";
+import {useState} from 'react'
 import Form from "react-bootstrap/Form"
 import {Button} from "react-bootstrap";
 
-class PollVoteForm extends React.Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            votes: {}
+function PollVoteForm(props) {
+    const [votes, setVotes] = useState(new Map(props.poll.choices.map((choice) => {
+            return [choice.id, false]
         }
-        for (let opt of props.poll.choices) {
-            this.state.votes[opt.id] = {isCast: false}
-        }
+    )))
 
-        this.handleInputChange = this.handleInputChange.bind(this)
-    }
+    console.log(votes)
 
-
-    handleInputChange(event) {
-        const choiceId = event.target.name
-        const checkboxValue = event.target.checked
-        this.setState((state, props) => {
-            let newVotes = {}
-            Object.assign(newVotes, state.votes)
-            newVotes[choiceId] = checkboxValue
-            return {votes: newVotes}
-        })
-    }
-
-    handleSubmission(event) {
-        console.log("Submitted!")
-    }
-
-
-    render() {
-        // console.log(this.state)
-        return (
-            <Form onSubmit={(e) => {e.preventDefault(); this.handleSubmission(e)}}>
-                <Form.Group controlId="formVoteSelection">
-                    <Form.Label>Vote Selection</Form.Label>
-                    {
-                        this.props.poll.choices.map((o) => {
-                            return <Form.Check
-                                name={o.id}
-                                key={o.id}
-                                label={o.text}
-                                checked={this.state.votes[o.id].isCast}
-                                onChange={this.handleInputChange}/>
-                        })
-                    }
-                </Form.Group>
-                <Button variant="warning" type="submit">
-                    Submit
-                </Button>
-            </Form>
-        )
-    }
+    return (
+        <Form onSubmit={(e) => {
+            e.preventDefault();
+        }}>
+            <Form.Group controlId="formVoteSelection">
+                <Form.Label>Vote Selection</Form.Label>
+                {
+                    props.poll.choices.map((choice) => {
+                        return <Form.Check
+                            name={choice.id}
+                            key={choice.id}
+                            label={choice.text}
+                            checked={votes[choice.id]}
+                            onChange={(event) => {
+                                const newVotes = new Map(votes)
+                                newVotes.set(event.target.name, event.target.checked)
+                                setVotes(newVotes)
+                            }}
+                        />
+                    })
+                }
+            </Form.Group>
+            <Button variant="warning" type="submit">
+                Submit
+            </Button>
+        </Form>
+    )
 }
 
 export default PollVoteForm
