5f726423f549d4fc605f918f3c2e8ff392760e92
Alex Eshoo <AlexEshoo@gmail.com>
2020-06-16 16:54:14 -0400

    finally got the login form working with formik (no mutation yet)

--- src/Components/LogInForm.js
+++ src/Components/LogInForm.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Formik, Form, Field, ErrorMessage} from "formik";
+import {Formik, Form, Field, ErrorMessage, useFormikContext} from "formik";
 import * as Yup from 'yup'
 import FormControl from 'react-bootstrap/FormControl'
 import FormCheck from 'react-bootstrap/FormCheck'
@@ -21,21 +21,45 @@ function LogInForm() {
             })}
             onSubmit={(values, {setSubmitting}) => console.log(values)}
         >
-            <Form>
-                <FormGroup>
-                    <Field name="username" placeholder="Username" as={FormControl}/>
-                    <ErrorMessage name="username"/>
-                </FormGroup>
-                <FormGroup>
-                    <Field name="password" type="password" placeholder="Password" as={FormControl}/>
-                    <ErrorMessage name="password"/>
-                </FormGroup>
-                <FormGroup>
-                    <Field name="rememberMe" as={FormCheck} label="Remember Me"/>
-                </FormGroup>
-                <Button type="submit" variant="success">Log In</Button>
-            </Form>
-
+            {/*Render Props pattern...*/}
+            {({isValid, errors, touched}) => (
+                <Form>
+                    <FormGroup>
+                        <Field
+                            name="username"
+                            as={FormControl}
+                            type="text"
+                            placeholder="Username"
+                            isInvalid={errors.username && touched.username}
+                        />
+                        <ErrorMessage name="username"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Field
+                            name="password"
+                            as={FormControl}
+                            type="password"
+                            placeholder="Password"
+                            isInvalid={errors.password && touched.password}
+                        />
+                        <ErrorMessage name="password"/>
+                    </FormGroup>
+                    <FormGroup>
+                        <Field
+                            name="rememberMe"
+                            as={FormCheck}
+                            label="Remember Me"
+                        />
+                    </FormGroup>
+                    <Button
+                        type="submit"
+                        variant="success"
+                        disabled={!isValid || Object.keys(touched).length === 0}
+                    >
+                        Log In
+                    </Button>
+                </Form>
+            )}
         </Formik>
     )
 }
