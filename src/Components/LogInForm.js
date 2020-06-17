d1d70d4c61ee81e6e5b482e90858ef27fb2e237d
Alex Eshoo <AlexEshoo@gmail.com>
2020-06-17 00:03:16 -0400

    hooked up log in mutation to button

--- src/Components/LogInForm.js
+++ src/Components/LogInForm.js
@@ -5,8 +5,39 @@ import FormControl from 'react-bootstrap/FormControl'
 import FormCheck from 'react-bootstrap/FormCheck'
 import FormGroup from 'react-bootstrap/FormGroup'
 import Button from "react-bootstrap/Button";
+import gql from 'graphql-tag';
+import {useMutation} from '@apollo/react-hooks';
+import Spinner from "react-bootstrap/Spinner";
+
+const LOGIN = gql`
+    mutation loginUser($username: String! $password: String! $rememberMe: Boolean) {
+        login(username: $username, password: $password, rememberMe: $rememberMe) {
+            ok
+            failReason
+        }
+    }
+`
 
 function LogInForm() {
+    const [login, loginResult] = useMutation(LOGIN)
+
+    let loginResultDisplay = null
+
+    if (loginResult.called) {
+        if (loginResult.loading) {
+            loginResultDisplay = <Spinner animation={"border"} variant="primary"/>
+        } else {
+            console.log(loginResult.data)
+            if (loginResult.data.login.ok) {
+                // do something
+            } else {
+                loginResultDisplay = <div>{loginResult.data.login.failReason}</div>
+            }
+        }
+    }
+
+    console.log(loginResultDisplay)
+
     return (
         <Formik
             initialValues={{
@@ -19,7 +50,18 @@ function LogInForm() {
                 password: Yup.string().required("Required"),
                 rememberMe: Yup.bool()
             })}
-            onSubmit={(values, {setSubmitting}) => console.log(values)}
+            onSubmit={(values, {setSubmitting}) => {
+                login(
+                    {
+                        variables: {
+                            username: values.username,
+                            password: values.password,
+                            rememberMe: values.rememberMe
+                        }
+                    }
+                )
+                setSubmitting(false)
+            }}
         >
             {/*Render Props pattern...*/}
             {({isValid, errors, touched}) => (
@@ -51,13 +93,18 @@ function LogInForm() {
                             label="Remember Me"
                         />
                     </FormGroup>
-                    <Button
-                        type="submit"
-                        variant="success"
-                        disabled={!isValid || Object.keys(touched).length === 0}
-                    >
-                        Log In
-                    </Button>
+                    <div className="d-flex align-content-center">
+                        <Button
+                            type="submit"
+                            variant="success"
+                            disabled={!isValid || Object.keys(touched).length === 0}
+                        >
+                            Log In
+                        </Button>
+                        <div style={{"padding-left": "10px"}}>
+                            {loginResultDisplay}
+                        </div>
+                    </div>
                 </Form>
             )}
         </Formik>
