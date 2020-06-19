380ba7f3078ee395a325b250fb0d88c99c281ebd
Alex Eshoo <AlexEshoo@gmail.com>
2020-06-18 22:22:23 -0400

    logout

--- src/Components/AppNavbar.js
+++ src/Components/AppNavbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import Navbar from "react-bootstrap/Navbar";
 import {Link} from "react-router-dom";
 import NavbarCollapse from "react-bootstrap/NavbarCollapse";
@@ -8,8 +8,39 @@ import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import Popover from "react-bootstrap/Popover";
 import Button from "react-bootstrap/Button";
 import LogInForm from "./LogInForm";
+import {UserContext} from "./UserContext";
+import LogoutButton from "./LogoutButton";
 
 function AppNavbar() {
+    const userContext = useContext(UserContext)
+
+    let loginControl
+    if (userContext.currentUser) {
+        loginControl = (
+            <div>
+                Logged in as {userContext.currentUser.username}
+                <LogoutButton/>
+            </div>
+        )
+    } else {
+        loginControl = (
+            <OverlayTrigger
+                trigger="click"
+                placement="bottom"
+                overlay={
+                    <Popover id="login-popover">
+                        <Popover.Content>
+                            <LogInForm/>
+                        </Popover.Content>
+                    </Popover>
+                }>
+                <Button>
+                    LOGIN
+                </Button>
+            </OverlayTrigger>
+        )
+    }
+
     return (
         <Navbar className="poll-nav">
             <Navbar.Brand as={Link} to="/">
@@ -21,20 +52,7 @@ function AppNavbar() {
                         New Poll
                     </NavItem>
                 </Nav>
-                <OverlayTrigger
-                    trigger="click"
-                    placement="bottom"
-                    overlay={
-                    <Popover id="login-popover">
-                        <Popover.Content>
-                            <LogInForm/>
-                        </Popover.Content>
-                    </Popover>
-                }>
-                    <Button>
-                        LOGIN
-                    </Button>
-                </OverlayTrigger>
+                {loginControl}
             </NavbarCollapse>
         </Navbar>
     )
